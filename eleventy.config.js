56b6c274a6a3f541ea9398003c1a097353dde17a
Guirdo <produccionesaldo@gmail.com>
2023-05-17 15:57:28 -0600

    🔧 Add css template language

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,7 +1,13 @@
+const glob = require('fast-glob')
 const pluginWebc = require("@11ty/eleventy-plugin-webc")
 const { EleventyI18nPlugin } = require("@11ty/eleventy")
 
 module.exports = function (eleventyConfig) {
+
+  glob.sync('./config/languages/*.js').forEach((file) => {
+    let plugin = require('./' + file)
+    eleventyConfig.addPlugin(plugin)
+  });
 
   eleventyConfig.addPlugin(pluginWebc, {
     components: "src/_includes/components/**/*.webc"
@@ -10,6 +16,8 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(EleventyI18nPlugin, {
     defaultLanguage: "en",
   });
+
+  eleventyConfig.addWatchTarget('./src/assets');
   
   return {
     dir: {
